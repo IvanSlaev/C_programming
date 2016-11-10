d79caa1c6931bf0a8d8288dcbb869d05d467168c
Ivan Slaev <islaev@lin-0225.ent.mm-sol.com>
2016-11-10 17:03:37 +0200

    folder

---
+++ linux_device_drivers/character_devices/my_char_dev/testit/test_the_char_device.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define DEATH(mess) { perror(mess); exit(errno); }
+
+int main(int argc, const char *argv[])
+{
+	int length, fd1, fd2, rc;
+	const char *nodename = "/dev/mycdrv";
+	char message[] = "THIS MESSAGE IS USED AS TEST FOR THE CHARACTER DRIVER";
+
+	length = sizeof(message);
+
+	fd1 = open(nodename, O_RDWR);
+	printf("%s%d\n", "first open file descriptor:", fd1);
+	fd2 = open(nodename, O_RDWR);
+	printf("%s%d\n", "second open file descriptor:", fd1);
+	
+	rc = write(fd1, message, length);
+	printf("%s%d%s%d\n", "return from write:", rc, " on file descriptor:", fd1);
+	memset(message, 0, length);
+	
+	rc = read(fd2, message, length);
+	printf("%s%d%s%d\n", "return from read:", rc, " on file descriptor:", fd2);
+	printf("%s%s\n", "Message:", message);
+	return 0;
+}
