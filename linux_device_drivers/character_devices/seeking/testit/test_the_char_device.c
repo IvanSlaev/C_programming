c5d1b4da020e7c9231594944bddaa6253cee2217
Ivan Slaev <islaev@lin-0225.ent.mm-sol.com>
2016-11-11 14:12:26 +0200

    edit test_char

--- linux_device_drivers/character_devices/seeking/testit/test_the_char_device.c
+++ linux_device_drivers/character_devices/seeking/testit/test_the_char_device.c
@@ -9,23 +9,31 @@
 
 int main(int argc, const char *argv[])
 {
-	int length, fd1, fd2, rc;
-	const char *nodename = "/dev/mycdrv";
-	char message[] = "THIS MESSAGE IS USED AS TEST FOR THE CHARACTER DRIVER";
+	int length = 20, position = 0, fd, rc;
+	char *message, *nodename = "/dev/mycdrv";
 
-	length = sizeof(message);
-
-	fd1 = open(nodename, O_RDWR);
-	printf("%s%d\n", "first open file descriptor:", fd1);
-	fd2 = open(nodename, O_RDWR);
-	printf("%s%d\n", "second open file descriptor:", fd1);
-	
-	rc = write(fd1, message, length);
-	printf("%s%d%s%d\n", "return from write:", rc, " on file descriptor:", fd1);
+// the message
+	message = malloc(length);
+	memset(message, 'x', length);
+	message[length - 1] = '\0';
+// oppening device node
+	fd = open(nodename, O_RDWR);
+	printf("%s%d\n", "open file descriptor:", fd);
+// seeking
+	rc = lseek(fd, position, SEEK_SET);
+	printf("%s%d\n", "Returned from lseek=", rc);
+// writeing
+	rc = write(fd, message, length);
+	printf("%s%d%s%d\n", "return from write:", rc, " on file descriptor:", fd);
+// reset message to null
 	memset(message, 0, length);
-	
-	rc = read(fd2, message, length);
-	printf("%s%d%s%d\n", "return from read:", rc, " on file descriptor:", fd2);
+// seeking
+	rc = lseek(fd, position, SEEK_SET);
+	printf("%s%d\n", "Returned from lseek=", rc);
+// reading
+	rc = read(fd, message, length);
+	printf("%s%d%s%d\n", "return from read:", rc, " on file descriptor:", fd);
 	printf("%s%s\n", "Message:", message);
+	close(fd);
 	return 0;
 }
