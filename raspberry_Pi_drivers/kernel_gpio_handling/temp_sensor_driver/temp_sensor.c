78152b3f9796dccd915afad2256948a6bc2b3702
Ivan Slaev <islaev@lin-0225.ent.mm-sol.com>
2017-03-14 14:35:00 +0200

    only reset the sensor

--- raspberry_Pi_drivers/kernel_gpio_handling/temp_sensor_driver/temp_sensor.c
+++ raspberry_Pi_drivers/kernel_gpio_handling/temp_sensor_driver/temp_sensor.c
@@ -1,64 +1,78 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/types.h>
 #include <linux/gpio.h>
 #include <linux/delay.h>
 #include <linux/workqueue.h>
-#include <linux/irq.h>
-#include <linux/interrupt.h>
-#include <linux/irqreturn.h>
 
-#define RED 17
-#define BUTTON 18
-#define GREEN 27
-#define WQ_NAME "led_work_queue"
+#define READ_SCRATHPAD 0xBE
+
+#define PIN 4
+#define GPIO_NAME TEMP_PIN
+#define WQ_NAME "work_queue"
 #define WQ_HZ_DELAY HZ/10
-
-static int irq;
 
 static void work_handler(struct work_struct *work);
 
 static struct workqueue_struct *led_wq;
 static DECLARE_DELAYED_WORK(led_w, work_handler);
 
-static void work_handler(struct work_struct *work)
-{
-	pr_info("Button working\n");
-}
- 
-static irqreturn_t irq_handler_gpio(int irq, void *dev_id)
+static void bus_pin_init(void)
 {
 	int ret;
+	
+	/* Initialization */
+	ret = gpio_request(PIN, "GPIO_NAME");
+	if (ret) {
+		pr_err("request GPIO %d failed\n", PIN);
+	}
 
-	disable_irq_nosync(irq);
-	ret = queue_delayed_work(led_wq, &led_w, WQ_HZ_DELAY);
-
-	return IRQ_HANDLED;
+	/* Setting GPIO function */
+	ret = gpio_direction_input(PIN);
+	if (ret) {
+		pr_err("gpio_direction_input GPIO %d failed\n", PIN);
+	}
 }
+
+static void master_reset(void)
+{
+	gpio_direction_output(PIN); 	/* pull the bus low */
+	mdelay(480);
+	gpio_direction_input(PIN);	/* release the bus */
+	mdelay(60); 			/* wait for the maximum DS18B20 reaction time */
+
+	if (gpio_get_value(PIN)) {
+		pr_info("DS18B20 reset successful\n");
+		return;
+	}
+	else {
+		pr_info("DS18B20 NOT reset, try again in 1 second\n");
+		sleep(1000);		/* wait for the maximum DS18B20 signal time before retry reset */
+		master_reset();		/* try again */
+	}
+}
+
+/*
+static uint_8 read_bit(void)
+{
+	
+}	
+
+static void work_handler(struct work_struct *work)
+{
+
+}
+*/
 
 static int __init my_init(void)
 {
 	int ret;
 	pr_info("Module Raspberry Pi\n");
 	
-	/* Initialization */
-	ret = gpio_request(BUTTON, "BUTTON");
-	if (ret) {
-		pr_err("request GPIO %d failed\n", BUTTON);
-	}
+	bus_pin_init();
 
-	/* Setting GPIO function */
-
-	ret = gpio_direction_input(BUTTON);
-	if (ret) {
-		pr_err("gpio_direction_input GPIO %d failed\n", BUTTON);
-	}
-
-	irq = gpio_to_irq(BUTTON);
-	if (!irq) {
-		pr_err("gpio_to_irq failed \n");
-	}	
-	
+	/*
 	led_wq = create_singlethread_workqueue(WQ_NAME);
 	if (led_wq) {
 		pr_info("WQ created\n");
@@ -66,11 +80,8 @@ static int __init my_init(void)
 		pr_err("No workqueue created\n");
 	}
 
-	ret = request_irq(irq, irq_handler_gpio, IRQF_TRIGGER_FALLING | IRQF_ONESHOT, "button", NULL);
-	if (ret) {
-		pr_err("irq request failed\n");
-	}
-
+	ret = queue_delayed_work(led_wq, &led_w, WQ_HZ_DELAY);
+	*/
 	return 0;
 }
 
@@ -78,12 +89,11 @@ static void __exit my_exit(void)
 {
 	pr_info("Goodbye, Module Raspberry Pi\n");
 
-	gpio_free(BUTTON);
-
+	gpio_free(PIN);
+	/*
 	cancel_delayed_work_sync(&led_w);
-
 	destroy_workqueue(led_wq);
-	free_irq(irq, NULL);
+	*/
 }
 
 module_init(my_init);
